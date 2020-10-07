03c7c60f07260aaaa8197506d39eacccd2930157
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2020-10-07 20:13:29 +0200

    react router

--- apireact/src/App.js
+++ apireact/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import MainRouter from './MainRouter';
 
-function App() {
-  return <div className='App'>Visszajövök</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <MainRouter />
+    </Router>
+  );
+};
 
 export default App;
---
+++ apireact/src/core/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => (
+  <div className='bg-light px-4 py-5'>
+    <div className='container'>
+      <h2>Home</h2>
+      <div className='lead'>Welcome to react frontend</div>
+    </div>
+  </div>
+);
+
+export default Home;
